2d217f667156c254a7dc1e81fd41dcbedc27c434
andrewcbuensalida <andrewcbuensalida@gmail.com>
2021-09-17 15:08:28 -0700

    overhauled the appspec and scripts

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 #download node and npm
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
-. ~/.nvm/nvm.sh
-nvm install node
+# curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+# . ~/.nvm/nvm.sh
+# nvm install node
 
 #create our working directory if it doesnt exist
-DIR="/home/ubuntu/node-mongodb-todo"
-if [ -d "$DIR" ]; then
-  echo "${DIR} exists"
-else
-  echo "Creating ${DIR} directory"
-  mkdir ${DIR}
+export FOLDER=/tmp/todo
+if [ -d $FOLDER ]
+then
+  rm -rf $FOLDER
 fi
+
+mkdir -p $FOLDER
