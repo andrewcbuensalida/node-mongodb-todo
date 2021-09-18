8da3edb4f8277ed3e75084ea8df6386d2e1024ce
andrewcbuensalida <andrewcbuensalida@gmail.com>
2021-09-17 20:44:00 -0700

    his scipts to scripts

--- myscripts/application_start.sh
+++ myscripts/application_start.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 
 #give permission for everything in the express-app directory
-sudo chmod -R 777 /home/ec2-user/express-app
+sudo chmod -R 777 /home/ubuntu/todo
+# chmod +x ./scripts/*
 
 #navigate into our working directory where we have all our github files
-cd /home/ec2-user/express-app
+cd /home/ubuntu/todo
 
 #add npm and node to path
-export NVM_DIR="$HOME/.nvm"	
+export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm	
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
 
 #install node modules
 npm install
+# npm install forever -g
+
+# # install this for puppeteer on node on linux
+# sudo amazon-linux-extras install epel -y
+# sudo yum install -y chromium
 
 #start our node app in the background
-node app.js > app.out.log 2> app.err.log < /dev/null & 
+# node server.js > server.out.log 2> server.err.log < /dev/null & 
+forever start script.js
--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
-# chmod +x ./scripts/*
 #download node and npm
-# curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
-# . ~/.nvm/nvm.sh
-# nvm install node
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+. ~/.nvm/nvm.sh
+nvm install node
 
 #create our working directory if it doesnt exist
-export FOLDER=/home/ubuntu/todo
-if [ -d $FOLDER ]
-then
-  rm -rf $FOLDER
+DIR="/home/ec2-user/express-app"
+if [ -d "$DIR" ]; then
+  echo "${DIR} exists"
+else
+  echo "Creating ${DIR} directory"
+  mkdir ${DIR}
 fi
-
-mkdir -p $FOLDER
